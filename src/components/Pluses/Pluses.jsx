2aea633734455659c79d4717cd2f05230814a542
ArtemSpiz <artemijspizhav@gmail.com>
2024-05-12 11:29:44 +0300

    copy-trading-mobile

--- src/components/Pluses/Pluses.jsx
+++ src/components/Pluses/Pluses.jsx
@@ -10,8 +10,10 @@ const Pluses = ({
   return (
     <div className='pluses'>
       <img src={img} alt="Pluses" className='pluses-image' />
+      <div className='plus-text'>
       <h3 className='pluses-title'>{title}</h3>
       <p className="pluses-text">{text}</p>
+      </div>
     </div>
   )
 }
